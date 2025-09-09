29a1e45b2a3e54dd3873bb5a721adfc4363f77cf
vagomezl474 <valerie.gomezlara@edu.sait.ca>
2025-09-09 14:28:21 -0600

    done
    no more updates needed

--- app/page.js
+++ app/page.js
@@ -12,7 +12,6 @@ export default function Home()
       <h1>CPRG306 Assignments</h1>
       <ul>
         <li><Link href="./week-2">Week 2 Assignment</Link></li>
-        <StudentInfo/>
       </ul>
     </main>
   );
