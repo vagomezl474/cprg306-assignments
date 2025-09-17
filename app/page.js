5850b0a23251259522b540265ac459cc2da6430f
vagomezl474 <valerie.gomezlara@edu.sait.ca>
2025-09-16 20:12:26 -0600

    link

--- app/page.js
+++ app/page.js
@@ -12,6 +12,7 @@ export default function Home()
       <h1>CPRG306 Assignments</h1>
       <ul>
         <li><Link href="./week-2">Week 2 Assignment</Link></li>
+        <li><Link href="./week-3">Week 3 Assignment</Link></li>
       </ul>
     </main>
   );
