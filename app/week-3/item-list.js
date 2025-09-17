470b1c3aa5e3864f3796542444f92cb18725ae2a
vagomezl474 <valerie.gomezlara@edu.sait.ca>
2025-09-16 22:46:21 -0600

    trying

---
+++ app/week-3/item-list.js
@@ -0,0 +1,13 @@
+
+
+export default function ItemList()
+{
+
+
+return(
+    <main>
+
+    </main>
+);
+
+}
---
+++ app/week-3/item.js
@@ -0,0 +1,15 @@
+
+
+
+export default function Items()
+{
+
+
+
+return(
+    <main>
+    </main>
+);
+
+
+}
